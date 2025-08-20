0be3329a6beab78e99e701d7b2e706001dc0a887
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-08-19 22:33:43 -0300

    Update aula6.py
    agora adicionei a função que mostra que tipo de dado é a variável

--- aula6.py
+++ aula6.py
@@ -1,4 +1,5 @@
-#Exercício 5 crie uma variável que armazene algum texto e mostre este texto na tela
+#Exercício 5 crie uma variável que armazene algum texto e mostre este texto na tela e o tipo de dado
 
 versiculo = "Todas as coisas cooperam para o bem daqueles que amam a Deus"
 print(versiculo) 
+print(type(versiculo))
