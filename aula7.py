ee2decb3b7a8b62465383bc1ccf682115d15b4f5
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-08-19 22:53:53 -0300

    Create aula7.py
    nesse commit fiz o exercício 4 que consiste em criar duas variáveis com valores numéricos, atribuir a soma deste dois valores a outra variável e mostrar o resultado na tela

---
+++ aula7.py
@@ -0,0 +1,8 @@
+#Crie duas variáveis com valores numéricos
+#Atribua a soma deste dois valores a outra variável
+#Mostre na tela este valor
+
+num1 = 7
+num2 = 4
+soma = num1 + num2
+print("A soma de ",num1,"e ",num2, "é ", soma)
