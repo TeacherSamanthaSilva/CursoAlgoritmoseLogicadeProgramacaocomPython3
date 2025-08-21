2995cd8b6a347fddab4f6f98eaa86343f47bac96
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-08-20 22:57:08 -0300

    Create aula10.py
    nesse commit fiz a resolução do exercício 5 que consiste em criar 3 variáveis que recebem os seguintes valores x= True
    y = False
    z = False e realizei as seguintes comparações #x and y
    #not x
    #x and x
    #y and y
    #y or x
    #y and z
    #not y
    #x and z

---
+++ aula10.py
@@ -0,0 +1,24 @@
+#Exercício 6
+#Crie as seguintes variáveis x=True, y=False e z=False e
+#Faça as seguintes comparações 
+#x and y
+#not x
+#x and x
+#y and y
+#y or x
+#y and z
+#not y
+#x and z
+
+x= True
+y = False
+z = False
+
+print(x and y)
+print(not x)
+print(x and x)
+print(y and y)
+print(y or x)
+print(y and z)
+print(not z)
+print(x and z)
