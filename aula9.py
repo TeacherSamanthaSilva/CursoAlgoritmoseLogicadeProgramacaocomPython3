f7409df10a070b674acf00294c96d4d46c8e5fb1
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-08-20 22:27:32 -0300

    Create aula9.py
    nesse commit fiz a resolução do exercício 5 que consiste em criar quatro variáveis que recebem os seguintes valores #a=5, b=3, c=10,d=2 e realizar as seguintes comparações #a > b
    #c==a
    #d != a
    #a <= c
    #b > c
    #a != c
    #d >= a
    #d != b
    #d < c
    #b >= a

---
+++ aula9.py
@@ -0,0 +1,30 @@
+#Exercicio 5
+#Crie as seguintes variáveis
+#a=5, b=3, c=10,d=2
+#Realize as seguintes comparações 
+#a > b
+#c==a
+#d != a
+#a <= c
+#b > c
+#a != c
+#d >= a
+#d != b
+#d < c
+#b >= a
+
+a = 5
+b = 3
+c = 10
+d = 2
+
+print(a > b)
+print(c == a)
+print(d != a)
+print(a <= c)
+print(b > c)
+print(a != c)
+print(d >= a)
+print(d != b)
+print(d < c)
+print(b >= a)
