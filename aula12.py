e341bcc1cf0f3daffb9021605eb0ad642a7ff12f
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-08-21 23:33:35 -0300

    Create aula12.py
    nesse commit fiz a resolução do exercício 8 que consiste em criar uma variável chamada salário e outra variável chamada aumento e mostre na tela o valor do salário com o aumento

---
+++ aula12.py
@@ -0,0 +1,8 @@
+#crie uma variável chamada salário e outra variável chamada aumento e mostre na tela o valor do salário com o aumento
+
+salario = float(input("Digite o valor do salário do funcionário"))
+aumento = float(input("Digite o valor do aumento que será dado"))
+
+novosalario = salario + aumento
+
+print("O Salário atual deste funcionário é R$: ", novosalario)
