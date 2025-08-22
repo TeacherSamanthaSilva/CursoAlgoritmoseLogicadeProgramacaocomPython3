1c5c4c345f7a17312b0b3f83641bb9008b2088a6
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-08-22 20:40:33 -0300

    Update aula20.py
    nesse commit eu aprendi como colocar duas casas decimais usando f-strings

--- aula20.py
+++ aula20.py
@@ -1,7 +1,8 @@
 #Exercicio 16
 #crie um algoritmo que receba o valor do salario e o valor do aumento e mostra a soma dos dois
 
-salario = float(input("Digite o valor do salário"))
-aumento = float(input("Digite o valor do aumento"))
-novosalario = salario + aumento 
-print(f"O salário deste funcionário que antes era R${salario} recebeu um aumento de R${aumento} e agora é R${novosalario}")
+salario = float(input("Digite o valor do salário: "))
+aumento = float(input("Digite o valor do aumento: "))
+novosalario = salario + aumento
+
+print(f"O salário deste funcionário que antes era R${salario:.2f} recebeu um aumento de R${aumento:.2f} e agora é R${novosalario:.2f}")
