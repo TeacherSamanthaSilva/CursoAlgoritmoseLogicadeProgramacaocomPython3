e54af3bac0169b72943f9f9021f93f7ef8f502b4
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-08-22 18:00:29 -0300

    Create aula15.py
    nesse commit fiz a resolução do #Exercicio 11
    #Crie uma string dinamica que exiba o seu nome e a sua idade
    #crie uma string dinâmica que exiba a quantidade de dinheiro que você possui

---
+++ aula15.py
@@ -0,0 +1,11 @@
+#Exercicio 11
+#Crie uma string dinamica que exiba o seu nome e a sua idade
+#crie uma string dinâmica que exiba a quantidade de dinheiro que você possui
+
+nome = "Samantha"
+idade = 31
+dinheiro = 200
+
+print("Meu nome é %s" %nome)
+print("Eu tenho %i " %idade + "anos de idade")
+print("Eu tenho %.2f reais " % dinheiro)
