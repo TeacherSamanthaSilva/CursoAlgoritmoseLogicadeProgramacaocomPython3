dd74b4bc7617840edb179b47a41356d0298bf824
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-08-22 20:50:21 -0300

    Create aula21.py
    nesse commit fiz o #Exercício 17
    #crie um algoritmo que receba uma base e uma potência e exiba o resultado na tela

---
+++ aula21.py
@@ -0,0 +1,9 @@
+#Exercício 17
+#crie um algoritmo que receba uma base e uma potência e exiba o resultado na tela
+
+base = int(input("Digite o valor da base"))
+potencia = int(input("Digite o valor da potência"))
+
+calculo = base ** potencia 
+
+print(f"O resultado do valor da {base} elevado ao valor da {potencia} é {calculo}")
