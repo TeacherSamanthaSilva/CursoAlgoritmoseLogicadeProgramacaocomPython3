9b199de8d70455d3a29a32df13e525b0b5ee804f
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-08-22 20:51:28 -0300

    Update aula21.py
    nesse commit substitui potencia por expoente

--- aula21.py
+++ aula21.py
@@ -2,8 +2,8 @@
 #crie um algoritmo que receba uma base e uma potência e exiba o resultado na tela
 
 base = int(input("Digite o valor da base"))
-potencia = int(input("Digite o valor da potência"))
+expoente = int(input("Digite o valor da potência"))
 
-calculo = base ** potencia 
+calculo = base ** expoente
 
-print(f"O resultado do valor da {base} elevado ao valor da {potencia} é {calculo}")
+print(f"O resultado do valor da {base} elevado ao valor da {expoente} é {calculo}")
