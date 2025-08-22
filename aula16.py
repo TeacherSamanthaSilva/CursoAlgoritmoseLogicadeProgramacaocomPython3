49ebc43c3cb0160855752d2210be1a7f0db977fd
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-08-22 18:13:32 -0300

    Create aula16.py
    nesse commit criei um algoritmo que simula um aplicativo de controle de gastos

---
+++ aula16.py
@@ -0,0 +1,16 @@
+#Exercicio16
+#crie um algoritmo que simula um app de controle de gastos
+
+salario = 2500
+dizimo = 250
+sbb = 50
+oferta = 100
+reservaparairaigreja = 500
+caixinha = 1000
+
+print("Meu salário é %.2f" %salario)
+print("Por isso meu dízimo é %.2f" %dizimo)
+print("Me comprometo a doar %.2f " %sbb +  "para a SBB para que eles distribuam bíblias para quem precisa")
+print("Como oferta de gratidão vou dar %.2f " %oferta + "por mês para a comunidade família para o pai a igreja que escolhi congregar")
+print("Vou separar %.2f " %reservaparairaigreja + "na carteira da 99 para poder ir para a igreja")
+print("Vou guardar %.2f" %caixinha + "na caixinha do nubank")
