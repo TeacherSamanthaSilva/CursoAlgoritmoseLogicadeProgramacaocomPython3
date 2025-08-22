7ee1d466dda8d185cf8980132f09cb747c7c43c4
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-08-22 20:30:14 -0300

    Create aula19.py
    nesse commit fiz o # Exercicio 15
    # Crie duas variáveis que vão receber números, faça a conversão delas e exiba a soma desses dois números

---
+++ aula19.py
@@ -0,0 +1,8 @@
+# Exercicio 15
+# Crie duas variáveis que vão receber números, faça a conversão delas e exiba a soma desses dois números
+
+primeironumero = int(input("Digite o primeiro número: "))
+segundonumero = int(input("Digite o segundo número: "))
+soma = primeironumero + segundonumero
+
+print("A soma do %i e do %i é %i" % (primeironumero, segundonumero, soma))
