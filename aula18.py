3845a101de0cfdc2fcb5d576b7f1d4476b7846c4
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-08-22 20:10:28 -0300

    Create aula18.py
    nesse commit fiz o #Exercicio 14
    #Crie um programa que receba o seu nome como dado e imprima o seu nome com uma saudação
    #utilizando a forma de string dinâmica

---
+++ aula18.py
@@ -0,0 +1,6 @@
+#Exercicio 14
+#Crie um programa que receba o seu nome como dado e imprima o seu nome com uma saudação 
+#utilizando a forma de string dinâmica
+
+nome = str(input("Qual é o seu nome? "))
+print("Seja bem vindo(a) %s" %nome)
