8550a958b4590c0a7e6c1b4cf25ee292d8281d19
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-08-22 21:47:38 -0300

    Create aula23.py
    nesse commit fiz #Exercicio 20
    #crie um algoritmo que leia uma idade é se for maior de 18 diga você é maior de idade senão diga você é menor de idade

---
+++ aula23.py
@@ -0,0 +1,9 @@
+#Exercicio 20
+#crie um algoritmo que leia uma idade é se for maior de 18 diga você é maior de idade senão diga você é menor de idade
+
+idade = int(input("Digite a sua idade "))
+
+if (idade >=18):
+    print("Você é maior de idade")
+else:
+    print("Você é menor de idade")
