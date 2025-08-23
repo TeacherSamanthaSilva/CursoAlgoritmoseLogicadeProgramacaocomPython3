95cebe5221c6de4c847609fc730c6bffda9c8e3b
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-08-23 17:21:25 -0300

    Update aula30.py
    atualizei o código para usar match true que é similar a switch case

--- aula30.py
+++ aula30.py
@@ -1,15 +1,16 @@
 #Exercício 27
 #crie um programa que leia a renda de um usuário e de acordo com a renda mencionada mostre qual será o limite esse cliente receberá. 
 
-renda = float(input("Digite a sua renda "))
+renda = float(input("Digite a sua renda: "))
 
-if renda < 2000:
-    print("Sua renda é abaixo do mínimo para possuir este cartão de crédito")
-elif renda >= 2000:
-    print("Você receberá um limite de 1000 reais")
-elif renda >= 4000: 
-    print("Você receberá um limite de 2000 reais")
-elif renda >= 6000 and renda <= 10000:
-    print("Você receberá um limite te de 3000 reais")
-elif renda >=10000:
-    print("Você tem um perfil para cartões Black fale com nosso gerente")
+match True:
+    case _ if renda < 2000:
+        print("Sua renda é abaixo do mínimo para possuir este cartão de crédito")
+    case _ if renda >= 10000:
+        print("Você tem um perfil para cartões Black, fale com nosso gerente")
+    case _ if renda >= 6000:
+        print("Você receberá um limite de 3000 reais")
+    case _ if renda >= 4000:
+        print("Você receberá um limite de 2000 reais")
+    case _ if renda >= 2000:
+        print("Você receberá um limite de 1000 reais")
