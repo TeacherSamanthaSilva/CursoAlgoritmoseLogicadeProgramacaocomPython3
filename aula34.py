77bfd1a18939745572636903cd81532d60a05b18
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-08-23 22:50:23 -0300

    Create aula34.py
    nesse commit fiz o #exercicio 5
    #crie uma função que faça a multiplicação de dois números
    #este números devem ser passados por parâmetro

---
+++ aula34.py
@@ -0,0 +1,14 @@
+#exercicio 5
+#crie uma função que faça a multiplicação de dois números
+#este números devem ser passados por parâmetro
+
+def multiplicar(num1, num2):
+    return num1 * num2
+
+# Programa principal
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+
+resultado = multiplicar(n1, n2)
+
+print(f"A multiplicação de {n1} x {n2} é: {resultado}")
